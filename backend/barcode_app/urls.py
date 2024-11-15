1c0ff605fd90a3442c9dec020bceb0f99b0536d7
Puneet kumar <73667955+puneet2121@users.noreply.github.com>
2024-11-15 19:04:57 +0530

    remove barcode

--- backend/barcode_app/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import BarcodeListCreateView, ProductListCreateView, BarcodeCreateView
-
-urlpatterns = [
-    path('barcodes/', BarcodeListCreateView.as_view(), name='barcode-list'),
-    path('products/', ProductListCreateView.as_view(), name='product-list'),
-    path('add-barcode/', BarcodeCreateView.as_view(), name='add-barcode'),
-]
