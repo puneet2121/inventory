14c11c2ce53a6df7e8f59cdaa4b84f622400c929
Puneet kumar <73667955+puneet2121@users.noreply.github.com>
2025-08-25 14:59:22 -0400

    add new db info

--- system/development/settings.py
+++ system/development/settings.py
@@ -1,7 +1,6 @@
 import os
 
 from system.base_settings import *
-
 ROOT_URLCONF = 'system.development.urls'
 
 WSGI_APPLICATION = 'system.development.wsgi.application'
@@ -9,3 +8,24 @@ WSGI_APPLICATION = 'system.development.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 #
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'postgres',
+#         'USER': 'postgres.patucqkblstgphmpndrz',
+#         'PASSWORD': '$Shivji21219319',
+#         'HOST': 'aws-0-ap-southeast-1.pooler.supabase.com',
+#         'PORT': '6543',
+#     }
+# }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'defaultdb',
+        'USER':  os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': '25060',
+    }
+}
