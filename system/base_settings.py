5e6b4993bb3d8e2bdea9ae0783e08bb4fa4cc7c0
Puneet kumar <73667955+puneet2121@users.noreply.github.com>
2025-08-25 21:01:35 -0400

    remove unwanted files

--- system/base_settings.py
+++ system/base_settings.py
@@ -70,7 +70,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'app.core.tenant_middleware.TenantMiddleware',
 ]
 TEMPLATES = [
