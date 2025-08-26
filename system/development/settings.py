675148658b5d55717ba01dee1789e56993d96b89
Puneet kumar <73667955+puneet2121@users.noreply.github.com>
2025-08-26 00:03:40 -0400

    change the db

--- system/development/settings.py
+++ system/development/settings.py
@@ -8,27 +8,27 @@ WSGI_APPLICATION = 'system.development.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 #
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'postgres',
-        'USER': 'postgres.patucqkblstgphmpndrz',
-        'PASSWORD': '$Shivji21219319',
-        'HOST': 'aws-0-ap-southeast-1.pooler.supabase.com',
-        'PORT': '6543',
-    }
-}
-
 # DATABASES = {
 #     'default': {
 #         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'defaultdb',
-#         'USER':  os.getenv('DB_USER'),
-#         'PASSWORD': os.getenv('DB_PASSWORD'),
-#         'HOST': os.getenv('DB_HOST'),
-#         'PORT': '25060',
+#         'NAME': 'postgres',
+#         'USER': 'postgres.patucqkblstgphmpndrz',
+#         'PASSWORD': '$Shivji21219319',
+#         'HOST': 'aws-0-ap-southeast-1.pooler.supabase.com',
+#         'PORT': '6543',
 #     }
 # }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'defaultdb',
+        'USER':  os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': '25060',
+    }
+}
 
 
 
